92098f0960421086c2ec1ec097064354499123ea
vaggy <sarvagya6@gmail.com>
2021-07-19 02:22:15 +0530

    flask index

--- run.py
+++ run.py
@@ -7,5 +7,10 @@ env_name = os.environ['FLASK_ENV']
 app = create_app(env_name)
 
 if __name__ == '__main__':
+    if env_name == 'development':
+        from livereload import Server
+
+        server = Server(app.wsgi_app)
+        server.serve()
     # run app
     app.run()
