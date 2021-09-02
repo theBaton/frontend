2daea41bb07e9b9d08058d7d4d5ef149e3d217c2
vaggy <sarvagya6@gmail.com>
2021-09-02 22:10:03 +0530

    Update forms.py

--- src/views/forms.py
+++ src/views/forms.py
@@ -1,12 +1,12 @@
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, URL
 from ..models.UserModel import User
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
@@ -27,3 +27,13 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class ProfileEditForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    bio = StringField('Bio')
+    facebook = StringField('Facebook', validators=[URL()])
+    instagram = StringField('Instagram', validators=[URL()])
+    linkedin = StringField('Linkedin', validators=[URL()])
+    twitter = StringField('Twitter', validators=[URL()])
+    
