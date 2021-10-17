ba6eca2a2e74510ea8a5feb339c1e990369d87f7
vaggy <sarvagya6@gmail.com>
2021-10-17 10:43:26 -0400

    update

--- src/views/forms.py
+++ src/views/forms.py
@@ -1,4 +1,3 @@
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, URL
