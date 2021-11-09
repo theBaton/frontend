7b30be0093d4bd96f48ee2f0c8ca268ab6826a0a
vaggy <sarvagya6@gmail.com>
2021-11-09 01:30:23 -0500

    ok

--- src/views/forms.py
+++ src/views/forms.py
@@ -1,47 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, URL
-from ..models.UserModel import User
-
-
-class RegistrationForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    checklicense = BooleanField('I agree to the license terms.')
-    submit = SubmitField('Sign Up')
-
-    def validate_email(self, email):
-
-        user = User.query.filter_by(email=email.data).first()
-
-        if user:
-            raise ValidationError('That email is taken. Please login')
-
 
 class LoginForm(FlaskForm):
     
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
+    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
+    
+class RegisterForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
 
-class UploadForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    sub_title = StringField('Sub Title')
-    cover_image = StringField('Cover Image Link', validators=[DataRequired(), URL()])
-    cta_title = StringField('CTA Title', validators=[DataRequired()])
-    keywords = StringField('Keywords')
-    content = StringField('Contents', validators=[DataRequired()])
+class UpdateAccountForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = StringField('Profile Picture', validators=[DataRequired()])
+    submit = SubmitField('Update')
 
-class ProfileEditForm(FlaskForm):
+class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    bio = StringField('Bio')
-    facebook = StringField('Facebook', validators=[URL()])
-    instagram = StringField('Instagram', validators=[URL()])
-    linkedin = StringField('Linkedin', validators=[URL()])
-    twitter = StringField('Twitter', validators=[URL()])
-    
+    message = StringField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send')
