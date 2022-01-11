681b9faf92579b3ef5966d9edcf8053b0f89c0fb
vaggy <sarvagya6@gmail.com>
2022-01-11 16:39:15 -0500

    Update RunningText.jsx

--- sections/index/RunningText.jsx
+++ sections/index/RunningText.jsx
@@ -4,27 +4,14 @@ const RunningText = () => {
     return (
         <div className="tb-section padding-top-xlg-150 padding-bottom-xlg-150">
             <div className="tb-section-inner">
-            {/* Begin scrolling text
-                ========================== 
-                Change data-scroll-speed="*" to adjust scrolling speed.
-                Use class "scr-text-reverse" to reverse scrolling direction. 
-                Use class "scr-text-stroke" to enable text stroke style (no effect on smaller screens!). 
-                */}
             <div className="tb-scrolling-text scr-text-stroke font-italic" data-scroll-speed={15}>
-                <div className="tb-scrolling-text-inner text-dark-3" data-text="Social Advocacy & Legal Clinic! -">
-                Social Advocacy &amp; Legal Clinic! -
+                <div className="tb-scrolling-text-inner text-dark-3" data-text="Social & Legal Advocacy Clinic! -">
+                Social  &amp; Legal Advocacy Clinic! -
                 </div> {/* /.tb-scrolling-text-inner */}
             </div>
-            {/* End scrolling text */}
-            {/* Begin scrolling text
-                ========================== 
-                Change data-scroll-speed="*" to adjust scrolling speed.
-                Use class "scr-text-reverse" to reverse scrolling direction. 
-                Use class "scr-text-stroke" to enable text stroke style (no effect on smaller screens!). 
-                */}
             <div className="tb-scrolling-text scr-text-stroke scr-text-reverse font-italic" data-scroll-speed={15}>
-                <div className="tb-scrolling-text-inner text-dark-3" data-text="Social Advocacy & Legal Clinic! -">
-                Social Advocacy &amp; Legal Clinic! -
+                <div className="tb-scrolling-text-inner text-dark-3" data-text="Social & Legal Advocacy Clinic! -">
+                Social  &amp; Legal Advocacy Clinic! -
                 </div> {/* /.tb-scrolling-text-inner */}
             </div>
             {/* End scrolling text */}
